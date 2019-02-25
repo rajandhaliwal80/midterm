0e0d7584a4dab11d61998bd1912bf5b9a0a2b4af
Rajan Dhaliwal <rajandhaliwal80@gmail.com>
2019-02-25 17:39:55 -0500

    midterm

---
+++ server/app.js
@@ -0,0 +1,96 @@
+
+
+    // modules for node and express
+    let createError = require('http-errors');
+    let express = require('express');
+    let path = require('path');
+    let cookieParser = require('cookie-parser');
+    let logger = require('morgan');
+
+    // modules for authentication
+    let session = require('express-session');
+    let passport = require('passport');
+    let passportLocal = require('passport-local');
+    let localStrategy = passportLocal.Strategy;
+    let flash = require('connect-flash');
+
+
+    // import "mongoose" - required for DB Access
+    let mongoose = require('mongoose');
+    // URI
+    let config = require('./config/db');
+
+    mongoose.connect(process.env.URI || config.URI, { useNewUrlParser: true });
+
+    let mongoDB = mongoose.connection;
+    mongoDB.on('error', console.error.bind(console, 'Connection Error:'));
+    mongoDB.once('open', () => {
+        console.log("Connected to MongoDB...");
+    });
+
+
+    // define routers
+    let index = require('./routes/index'); 
+    let books = require('./routes/books'); 
+
+    let app = express();
+
+    // view engine setup
+    app.set('views', path.join(__dirname, 'views'));
+    app.set('view engine', 'ejs');
+
+    app.use(logger('dev'));
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: false }));
+    app.use(cookieParser());
+    app.use(express.static(path.join(__dirname, '../client')));
+
+    //setup express-session
+    app.use(session({
+        secret: "someSecrate",
+        saveUninitialized: false,
+        resave: false
+
+    }));
+
+    //initialize flash
+    app.use(flash());
+
+    //initializing passport
+    app.use(passport.initialize());
+    app.use(passport.session());
+
+    // create User model
+    let userModel = require('./models/user');
+    let User = userModel.User;
+
+    //implement a user authentication strategy
+    passport.use(User.createStrategy());
+
+    // serialize and deserialize the user info
+    passport.serializeUser(User.serializeUser());
+    passport.deserializeUser(User.deserializeUser());
+
+
+    // route redirects
+    app.use('/', index);
+    app.use('/books', books);
+
+
+    // catch 404 and forward to error handler
+    app.use(function(req, res, next) {
+        next(createError(404));
+    });
+
+    // error handler
+    app.use(function(err, req, res, next) {
+        // set locals, only providing error in development
+        res.locals.message = err.message;
+        res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+        // render the error page
+        res.status(err.status || 500);
+        res.render('error');
+    });
+
+    module.exports = app;
---
+++ server/routes/books.js
@@ -0,0 +1,152 @@
+    
+
+
+    // modules required for routing
+    let express = require('express');
+    let router = express.Router();
+    let mongoose = require('mongoose');
+    let passport = require('passport');
+
+    // define the book model
+    let book = require('../models/books');
+
+    //-------------------
+    function requiredAuth(req, res, next) {
+        //checking if the user is logged in
+        if (!req.isAuthenticated()) {
+            return res.redirect('/login');
+        }
+        next();
+    }
+
+    /* GET books List page. READ */
+
+    router.get('/', requiredAuth, (req, res, next) => {
+        // find all books in the books collection
+        book.find((err, books) => {
+            if (err) {
+                return console.error(err);
+            } else {
+                res.render('books/index', {
+                    title: 'Books',
+                    books: books,
+                    displayName: req.user ? req.user.displayName : ''
+                });
+            }
+        });
+
+    });
+
+    //  GET the Book Details page in order to add a new Book
+    router.get('/add', (req, res, next) => {
+
+        /*****************
+         * ADD CODE HERE *
+         *****************/
+        res.render('books/details', {
+            title: 'Add New Books',
+            books: '',
+            displayName: req.user ? req.user.displayName : ''
+        });
+
+    });
+
+
+
+    // POST process the Book Details page and create a new Book - CREATE
+    router.post('/add', (req, res, next) => {
+
+        /*****************
+         * ADD CODE HERE *
+         *****************/
+        let newBook = book({
+            "Title": req.body.title,
+            "Price": req.body.price,
+            "Author": req.body.author,
+            "Genre": req.body.genre
+        });
+        book.create(newBook, (err, book) => {
+            if (err) {
+                console.log(err);
+                res.end();
+            } else {
+                // Refresh the book list
+                res.redirect('/books');
+            }
+        });
+
+    });
+
+    // GET the Book Details page in order to edit an existing Book
+    router.get('/:id', (req, res, next) => {
+
+        /*****************
+         * ADD CODE HERE *
+         *****************/
+        let id = req.params.id;
+        book.findById(id, (err, bookObject) => {
+            if (err) {
+                console.log(err);
+                res.end(err);
+            } else {
+                // show the details of edit or delete results
+                res.render('books/details', {
+                    title: 'Edit Details Book',
+                    books: bookObject,
+                    displayName: req.user ? req.user.displayName : ''
+                });
+            }
+        });
+    });
+
+    // POST - process the information passed from the details form and update the document
+    router.post('/:id', (req, res, next) => {
+
+        /*****************
+         * ADD CODE HERE *
+         *****************/
+        let id = req.params.id;
+        let newBook = book({
+            "_id": id,
+            "Title": req.body.title,
+            "Price": req.body.price,
+            "Author": req.body.author,
+            "Genre": req.body.genre
+        });
+        book.update({ _id: id }, newBook, (err) => {
+            if (err) {
+                console.log(err);
+                res.end(err);
+            } else {
+                // Refresh favourite books-list
+                res.redirect('/books')
+            }
+        });
+
+    });
+
+    // GET - process the delete by user id
+    router.get('/delete/:id', (req, res, next) => {
+
+        /*****************
+         * ADD CODE HERE *
+         *****************/
+        let id = req.params.id;
+        //deleting methods 
+        book.remove({ _id: id }, (err) => {
+
+            if (err) {
+                console.log(err);
+                res.end(err);
+            } else {
+                //Refresh Favvourite List
+                res.redirect('/books');
+
+            }
+
+        });
+    });
+
+
+
+    module.exports = router;
